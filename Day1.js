44d5f579acf8afd8413248e180683e27c86df536
ASIF <asifiqbalk99@gmail.com>
2024-07-13 21:28:24 +0530

    Day1

---
+++ Day1.js
@@ -0,0 +1,32 @@
+// Activity 1
+var num1 = 10;
+let num2 = "Num2";
+console.log(num1);
+console.log(num2);
+
+// Activity 2
+const bol = true;
+console.log(bol);
+
+// Activity 3
+let num = 1;
+let str = "abc"
+let bool = false
+let obj = {name:'Asif'}
+let arr = [1,2,3,5,6]
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log((typeof(bool)));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+// Activity 4
+let anotherVar = 10
+console.log(`initial Val = ${anotherVar}`);
+
+anotherVar = 11
+console.log(`Updated Val = ${anotherVar}`);
+
+// Activity 5
+// bol = false; It generate error
